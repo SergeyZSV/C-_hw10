2459fc9c2681ee5af82dd715b4abe5929f4b3b21
SergeyZSV <sergey06062001@gmail.com>
2022-04-17 22:15:18 +0300

    Task 3

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// 3. Дано натуральное число n > 1. Вывести все простые множители данного числа.
+
+// 10 >> 5, 2.
+// 12 >> 2, 2, 3.
+
+int n = 12;
+SimpleDividers(n);
+
+void SimpleDividers (int currentNumber, int del = 1)
+{
+    if (currentNumber == 1) return;
+    del++;
+    if (currentNumber % del == 0) 
+    {
+        Console.Write(del + " ");
+        currentNumber /= del;
+        del = 1;
+    }
+    SimpleDividers(currentNumber,del);
+}
+
+
