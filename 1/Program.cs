ac2613223a72669fdaf77aceef2a92191910bb5b
SergeyZSV <sergey06062001@gmail.com>
2022-04-17 16:48:57 +0300

    Task 1

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Дано число n. Получите число, записанное в обратном порядке.
+
+// 345 >> 543
+// 81 >> 18
+
+Random rnd = new Random();
+int n = rnd.Next(10, 1000);
+Console.WriteLine($"Дано число {n}.");
+Console.WriteLine($"Число в перевернутом виде: {NumberReverse(n)}.");
+
+
+string NumberReverse(int number, string reversedNumber = "")
+{
+    if (number == 0) return reversedNumber;
+    reversedNumber += number % 10;
+    return NumberReverse(number / 10, reversedNumber);
+}
